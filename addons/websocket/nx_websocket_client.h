d5a34aa078317a54858a172fb48734df91e1acfa
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2025-09-29 16:39:27 +0100

    Fixed typo in updated _nx_websocket_client_connect declaration.

--- addons/websocket/nx_websocket_client.h
+++ addons/websocket/nx_websocket_client.h
@@ -297,7 +297,7 @@ UINT  _nx_websocket_client_connect(NX_WEBSOCKET_CLIENT *client_ptr, NX_TCP_SOCKE
                                    UCHAR *host, UINT host_length,
                                    UCHAR *uri_path, UINT uri_path_length,
                                    UCHAR *protocol, UINT protocol_length,
-                                   *bearer, UINT bearer_length, UINT wait_option);
+                                   UCHAR *bearer, UINT bearer_length, UINT wait_option);
 #ifdef NX_SECURE_ENABLE
 UINT  _nxe_websocket_client_secure_connect(NX_WEBSOCKET_CLIENT *client_ptr, NX_SECURE_TLS_SESSION *tls_session,
                                            UCHAR *host, UINT host_length,
